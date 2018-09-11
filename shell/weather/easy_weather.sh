e5d49603c4461ee8f61fc79a91523e0d46c08c81
yamozha <yamozha@localhost.localdomain>
2018-09-12 00:28:22 +0300

    updated easy

--- shell/weather/easy_weather.sh
+++ shell/weather/easy_weather.sh
@@ -1,11 +1,11 @@
 echo "Do you want to see the weather or the state of the moon? (1 or 2)"
 echo "Number:"; read "Foremoon"
-
+if $Foremoon == "1"; echo "Please specify your city";read "City"
 
 if $Foremoon == "2"
-   then "curl wttr.in/Moon"
+   then curl wttr.in/Moon
 if $Foremoon == "1"
-   then "curl wttr.in/~Sofia"
+   then curl wttr.in/~$City
 
 echo "hope you have nice day!!!"
 
