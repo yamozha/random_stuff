3f8ccf3706a7da1b51be977f1275921fc3310996
yamozha <yamozha@localhost.localdomain>
2018-09-14 16:16:39 +0300

    made it look cooler

--- shell/nmap.sh
+++ shell/nmap.sh
@@ -1,6 +1,7 @@
 echo -e "\e[33mSpecify your gateway"
 echo -e "\e[0m192.168.?.*"; read "Gateway"
+echo -e "\e[5m\e[101m\e[1mResults\e[0m"
+echo -e "------------------------------"
+sudo nmap -sn 192.168.$Gateway.* 
 
-sudo nmap -sn 192.168.$Gateway.* > result.txt
-
-echo -e "\e[33mResults written in result.txt!!"
+echo -e "------------------------------"
