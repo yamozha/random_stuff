bc1b024e32605630019d6b1075a33725780cb943
Yamozha <yyamozha@gmail.com>
2018-10-06 19:52:17 +0000

    added beebot project

---
+++ BeeBot/bot.py
@@ -0,0 +1,42 @@
+import discord
+import aiohttp
+from discord.ext import commands
+import asyncio
+
+description = '''beebot'''
+bot = commands.Bot(command_prefix='b?', description=description)
+
+
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def nasty():
+     await bot.say("yeet")
+
+
+@bot.listen()
+async def on_message(message):
+     if "bee" in message.content:
+         await bot.send_file(message.channel,"./bee.png")
+
+
+@bot.listen()
+async def on_message(message):
+    if "thanos car" in message.content:
+	    await bot.say("fuck you")
+
+@bot.command()
+async def code():
+    await bot.say("https://pastebin.com/6Wgh7Ua1")
+    await bot.say("^code^")
+
+bot.run("NDk4MjAyMDQ3OTUxMTQyOTIz.DpqYmw.pwDF_e4h4Ty7Hf0skKH2PX-GxPQ")
+
+
+
