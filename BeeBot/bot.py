7e83e7b073f868edfc4cba1caa9b1d06ea3a3780
Yamozha <yyamozha@gmail.com>
2018-10-07 15:17:55 +0300

    Update BeeBot/bot.py

--- BeeBot/bot.py
+++ BeeBot/bot.py
@@ -42,13 +42,46 @@ async def code():
 async def on_member_join(member):
     with open("users.json", "r") as f:
         users = json.load(f)
+  
+    await update_data(users, member)
 
-
+    with open ("users.json", "w") as f:
+        json.dump(users,f)
 
 @bot.event
 async def on_message(message):
-    with open ("users.json", "w") as f:
+     with open("users.json", "r") as f:
+        users = json.load(f)
+     
+     await update_data(users, message.author)
+     await add_experience(users, message.author, 5)
+     await level_up(users, message.author, message.channel)
+
+     
+     
+     with open ("users.json", "w") as f:
         json.dump(users,f)
+    
+
+async def update_data(users, user):
+    if not user.id in users:
+        users[user.id] = {}
+        users[user.id]["experience"]=0
+        users[user.id]["level"] = 1
+
+async def add _experience(users, user, exp):
+    users[user.did]["experience"] += exp
+
+
+async def level_up(users, user, channel):
+    experience = users [user.id]["experience"]
+    level_start = users[user.id]["level"]
+    level_end = int(experience ** (1/4))
+
+
+    if level_start < level_end:
+        await bot.send_message(channel, "{} has bee'd up to bee {}").format(user.mention, level_end)
+        users[user.id]["level"] = level_end
 
 
 bot.run("NDk4MjAyMDQ3OTUxMTQyOTIz.DpqYmw.pwDF_e4h4Ty7Hf0skKH2PX-GxPQ")
