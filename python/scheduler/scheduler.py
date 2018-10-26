fb934305b2ea0c689bacc75b36b40ce0db2ecaaf
Yamozha <yyamozha@gmail.com>
2018-10-26 08:47:42 +0300

    new project scheduler.py

--- python/scheduler/scheduler.py
+++ python/scheduler/scheduler.py
@@ -4,82 +4,93 @@ import datetime
 from datetime import date
 import calendar
 import sys
+import time
 
-def what_class_do_i_have_next(current_time):
-    dateday = date.today()
-    date = calendar.day_name[dateday.weekday()]
-    time = (current_time.strftime('%H:%M'))
+# def what_class_do_i_have_next(current_time):
+dateday = date.today()
+date = calendar.day_name[dateday.weekday()]
+time = (time.strftime('%H:%M'))
 
 
-    # Mon = ("Monday")
-    # Tue = ("Tuesday")
-    # Wed = ("Wednesday")
-    # Thur = ("Thursday")
-    # Fri = ("Friday")
+mon = ("Monday")
+tue = ("Tuesday")
+wed = ("Wednesday")
+thur = ("Thursday")
+fri = ("Friday")
 
-    weekdays = {
-        "mon": "Monday",
-        "tue": "Tuesday",
-        "wed": "Wednesday",
-        "thur": "Thursday",
-        "fri": "Friday",
-        "sat": "Saturday",
-    }
+    # weekdays = {
+    #     "mon": "Monday",
+    #     "tue": "Tuesday",
+    #     "wed": "Wednesday",
+    #     "thur": "Thursday",
+    #     "fri": "Friday",
+    #     "sat": "Saturday",
+    # }
 
-    if date == weekdays['mon']:
+if date == mon:
+    if time > "8:30" < "10:00":
+        print("Business")
+    if time > "10:00" < "11:20":
+        print("History")
+    if time > "12:00" < "12:50":
+        print("IT")
+    if time > "12:50" < "13:30":
+        print("IT")
+    if time > "13:40" < "14:20":
+        print("Art")
+
+
+    elif date == tue:
         if time > "8:30" < "10:00":
-            return "Next Esp2"
+            print("BG")
         if time > "10:00" < "11:20":
-            return "Next Esp2"
+            print("Spanish")
         if time > "12:00" < "12:50":
-            return "Next Esp2"
+            print("English")
         if time > "12:50" < "13:30":
-            return "Next Esp2"
-        if time > "" < "":
+            print("English")
+        if time > "13:40" < "14:20":
+            print("Math")
 
-    elif date == :
-        if time > "8:30" < ""
-            return "Next Esp2"
-        if time > "12:00" < "12:40":
-            return "Next Esp2"
-        if time > "12:00" < "12:40":
-            return "Next Esp2"
-        if time > "12:00" < "12:40":
-            return "Next Esp2"
+    elif date == wed:
+        if time > "8:30" < "10:00":
+            print("Chem")
+        if time > "10:00" < "11:20":
+            print("History")
+        if time > "12:00" < "12:50":
+            print("BG")
+        if time > "12:50" < "13:30":
+            print("Spanish")
+        if time > "13:40" < "14:20":
+            print("Spanish")
 
-    elif date == Wed:
-        if time > "8:30" < ""
-            return "Next Esp2"
-        if time > "12:00" < "12:40":
-            return "Next Esp2"
-        if time > "12:00" < "12:40":
-            return "Next Esp2"
-        if time > "12:00" < "12:40":
-            return "Next Esp2"
+    elif date == thur:
+        if time > "8:30" < "9:50":
+            print("Phys")
+        if time > "10:00" < "11:20":
+            print("Maths")
+        if time > "12:00" < "12:50":
+            print("Chem")
+        if time > "12:50" < "13:30":
+            print("English")
+        if time > "13:40" < "14:20":
+            print("English")
 
-    elif date == Thur:
-        if time > "8:30" < ""
-            return "Next Esp2"
-        if time > "12:00" < "12:40":
-            return "Next Esp2"
-        if time > "12:00" < "12:40":
-            return "Next Esp2"
-        if time > "12:00" < "12:40":
-            return "Next Esp2"
-
-    elif date == Fri:
-        if time > "8:30" < ""
-            return "Next Esp2"
-        if time > "12:00" < "12:40":
-            return "Next Esp2"
-        if time > "12:00" < "12:40":
-            return "Next Esp2"
-        if time > "12:00" < "12:40":
-            return "Next Esp2"
+    elif date == fri:
+        if time > "8:30" < "10:00":
+            print("Geography")
+        if time > "10:00" < "11:20":
+            print("PE")
+        if time > "12:00" < "12:50":
+            print("Biology")
+        if time > "12:50" < "13:30":
+            print("Biology")
+        if time > "13:40" < "14:20":
+            print("none yeet")
 
     else:
-        return "you are free"
+        print("you are free")
 
 
-if __name__ == "__main__":
-    what_class_do_i_have_next(datetime.datetime.now())
+# if __name__ == "__main__":
+#     what_class_do_i_have_next(datetime.datetime.now())
